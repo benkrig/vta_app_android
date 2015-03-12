49de1a9d7d141197c3f7b1d024b62a1c9094d2df
Ben Krig <benkrig@Benjamins-MacBook-Pro.local>
2015-03-12 14:53:22 -0700

    dev

--- src/johankrig/hotmail/com/LocationSearchFragment.java
+++ src/johankrig/hotmail/com/LocationSearchFragment.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -22,34 +23,34 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import johankrig.hotmail.com.R;
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.location.Address;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Display;
 import android.view.Gravity;
 import android.view.InflateException;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.view.View.OnTouchListener;
-import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.PopupMenu;
 import android.widget.PopupWindow;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -72,6 +73,8 @@ public class LocationSearchFragment extends Fragment
 	private ImageButton button1;
 	private PopupWindow changeStatusPopUp;
 	
+	RelativeLayout touch ;
+	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) 
 	{
@@ -90,24 +93,24 @@ public class LocationSearchFragment extends Fragment
 	    catch (InflateException e) 
 	    {
 	        //map is already there, just return view as it is
-	    }
-	    
+	    }	    
 	    
 		map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.mainmap)).getMap();
         map.setMyLocationEnabled(true);
-        
+
         map.getUiSettings().setMyLocationButtonEnabled(true);
+        
         //Hide keyboard on map click
-        map.setOnMapClickListener(new OnMapClickListener()
+        /*map.setOnMapClickListener(new OnMapClickListener()
         {
 			@Override
 			public void onMapClick(LatLng point) 
 			{
 				hideKeyBoard();
 			}
-        });
+        });*/
         
-        map.setOnMapLongClickListener(new OnMapLongClickListener()
+        /*map.setOnMapLongClickListener(new OnMapLongClickListener()
         {
 			@Override
 			public void onMapLongClick(LatLng point) 
@@ -116,7 +119,7 @@ public class LocationSearchFragment extends Fragment
 				GetMarkerFromTouch getad = new GetMarkerFromTouch(getActivity(), point);
 				getad.execute();
 			}
-        });
+        });*/
                 
 		return rootView;
     }
@@ -143,37 +146,122 @@ public class LocationSearchFragment extends Fragment
 		comm = (Communicator) getActivity();
 		
 		
-		RelativeLayout touch = (RelativeLayout) getActivity().findViewById(R.id.touchLayout);
+		final Handler myHandler = new Handler();	
 		
+		touch = (RelativeLayout) getActivity().findViewById(R.id.touchlayout);
 		touch.setOnTouchListener(new OnTouchListener()
 		{
-
-			@Override
-			public boolean onTouch(View v, MotionEvent event) 
+			Vibrator vb = (Vibrator)   getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+            
+			MotionEvent event;
+			View v;
+			RelativeLayout purplelayout = new RelativeLayout(getActivity());
+			RelativeLayout greylayout = new RelativeLayout(getActivity());
+			RelativeLayout bluelayout = new RelativeLayout(getActivity());
+			boolean purple = false;
+			boolean blue = false;
+			boolean grey = false;
+			
+			Runnable r = new Runnable()
 			{
-				LinearLayout purplelayout = new LinearLayout(getActivity());
-				LinearLayout greylayout = new LinearLayout(getActivity());
-				LinearLayout bluelayout = new LinearLayout(getActivity());
-				
-				purplelayout.setBackgroundColor(getActivity().getResources().getColor(R.color.purple));
-				greylayout.setBackgroundColor(getActivity().getResources().getColor(R.color.buttondarkgrey));
-				bluelayout.setBackgroundColor(getActivity().getResources().getColor(R.color.exitblue));
+			    public void run()
+			    {
+			    	((RelativeLayout)v).setBackgroundColor(0xAA000000);
+		            vb.vibrate(50);
+			    	map.getUiSettings().setScrollGesturesEnabled(false);
+					purplelayout.setBackgroundResource(R.color.purple);
+					greylayout.setBackgroundColor(getActivity().getResources().getColor(R.color.buttondarkgrey));
+					bluelayout.setBackgroundColor(getActivity().getResources().getColor(R.color.exitblue));
+					
+					
+					int oh = ((RelativeLayout)v).getHeight() / 2;
+					int ow = ((RelativeLayout)v).getWidth() / 2;
+					
+					float x = 0;
+					float y = 0;
+					float x1 = 0;
+					float y1 = 0;
+					float x2 = 0;
+					float y2 = 0;
 
-				if(event.getAction() == MotionEvent.ACTION_DOWN)
-				{
-		            float x = event.getX()*event.getXPrecision() - 44;
-		            float y = event.getY()*event.getYPrecision() - 130;
+					
+					if(event.getY()*event.getYPrecision() >= oh)
+					{
+						//lower
+						
+						if(event.getX()*event.getXPrecision() > ow)
+						{
+							//right
+							
+							x = event.getX()*event.getXPrecision()- 10;
+				            y = event.getY()*event.getYPrecision() - 230;
 
 
-		            float x1 = event.getX()*event.getXPrecision() + 90;
-		            float y1 = event.getY()*event.getYPrecision() - 130;
+				            x1 = event.getX()*event.getXPrecision() - 180;
+				            y1 = event.getY()*event.getYPrecision() - 180;
 
 
-		            float x2 = event.getX()*event.getXPrecision() + 90;
-		            float y2 = event.getY()*event.getYPrecision() - 30;
-		            
+				            x2 = event.getX()*event.getXPrecision() - 180;
+				            y2 = event.getY()*event.getYPrecision();
+						}
+						else
+						{
+							//left
+							
+							x = event.getX()*event.getXPrecision() - 56;
+				            y = event.getY()*event.getYPrecision() - 250;
+
+
+				            x1 = event.getX()*event.getXPrecision() + 100;
+				            y1 = event.getY()*event.getYPrecision() - 200;
+
+
+				            x2 = event.getX()*event.getXPrecision() + 100;
+				            y2 = event.getY()*event.getYPrecision() - 44;
+						}
+					}
+					else
+					{
+						//upper						
+						if(event.getX()*event.getXPrecision() > ow)
+						{
+							//right
+							
+							//p
+							x = event.getX()*event.getXPrecision() - 190;
+				            y = event.getY()*event.getYPrecision() - 144;
+
+				            //b
+				            x1 = event.getX()*event.getXPrecision() - 190;
+				            y1 = event.getY()*event.getYPrecision();
+
+				            //g
+				            x2 = event.getX()*event.getXPrecision() - 44;
+				            y2 = event.getY()*event.getYPrecision() + 64;
+						}
+						else
+						{
+							//left
+							
+							//p
+							x = event.getX()*event.getXPrecision() + 90;
+				            y = event.getY()*event.getYPrecision() - 144;
+
+				            //b
+				            x1 = event.getX()*event.getXPrecision() + 90;
+				            y1 = event.getY()*event.getYPrecision();
+
+				            //g
+				            x2 = event.getX()*event.getXPrecision() - 44;
+				            y2 = event.getY()*event.getYPrecision() + 44;
+						}
+					}
+					
 		            
 		            RelativeLayout.LayoutParams bp2 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+		            Resources resources = getActivity().getResources();
+		            DisplayMetrics metrics = resources.getDisplayMetrics();
+		            
 		            bp2.leftMargin = (int) x2;
 		            bp2.topMargin = (int) y2;
 		            bp2.height = 88;
@@ -194,24 +282,114 @@ public class LocationSearchFragment extends Fragment
 		            bp1.height = 88;
 		            bp1.width = 88;
 		            bluelayout.setLayoutParams(bp1);
-		            ((RelativeLayout)v.getParent()).addView(purplelayout);
-		            ((RelativeLayout)v.getParent()).addView(bluelayout);
-		            ((RelativeLayout)v.getParent()).addView(greylayout);
 		            
-		            return true;
-				}
-				if(event.getAction() == MotionEvent.ACTION_UP)
+		            //ontouch listeners
+		            
+		            
+		            
+		            ((RelativeLayout)v).addView(purplelayout);
+		            ((RelativeLayout)v).addView(bluelayout);
+		            ((RelativeLayout)v).addView(greylayout);
+		            
+			    }
+			};
+
+			private float mDownX;
+			private float mDownY;
+			private final float SCROLL_THRESHOLD = 10;
+			private boolean isOnClick;
+					
+			@Override
+			public boolean onTouch(View v, MotionEvent event) 
+			{
+				if(event.getAction() == MotionEvent.ACTION_DOWN)
 				{
-					Toast.makeText(getActivity(), ""+((RelativeLayout)v.getParent()).getChildCount(), Toast.LENGTH_SHORT).show();
-					((RelativeLayout)v.getParent()).removeAllViews();
-		            rootView.invalidate();
-					Toast.makeText(getActivity(), ""+((RelativeLayout)v.getParent()).getChildCount(), Toast.LENGTH_SHORT).show();
-
-
+					this.v = v;
+					this.event = event;	
+					
+					myHandler.removeCallbacks(r);
+					myHandler.postDelayed(r, 1000);
+					hideKeyBoard();
+					mDownX = event.getX();
+		            mDownY = event.getY();
+		            isOnClick = true;
+					
 					return true;
 				}
-	            
-	            
+				
+				if(event.getAction() == MotionEvent.ACTION_MOVE)
+			    {
+
+					if (isOnClick && (Math.abs(mDownX - event.getX()) > SCROLL_THRESHOLD || Math.abs(mDownY - event.getY()) > SCROLL_THRESHOLD)) 
+					{
+		                Log.i("ay", "movement detected");
+		                myHandler.removeCallbacks(r);
+		                isOnClick = false;
+		            }
+					//myHandler.removeCallbacks(r);
+					if(isPointInsideView(event.getRawX(), event.getRawY(), purplelayout) && !purple)
+					{
+						purple = true;
+						grey = false;
+						blue = false;
+			            vb.vibrate(50);
+					}
+					else if(isPointInsideView(event.getRawX(), event.getRawY(), greylayout) && !grey)
+					{
+						purple = false;
+						grey = true;
+						blue = false;
+			            vb.vibrate(50);
+					}
+					else if(isPointInsideView(event.getRawX(), event.getRawY(), bluelayout) && !blue)
+					{
+						purple = false;
+						grey = false;
+						blue = true;
+			            vb.vibrate(50);
+					}
+					
+					return false;
+			    }
+				
+				if(event.getAction() == MotionEvent.ACTION_UP)
+				{
+		            ((RelativeLayout)v).setBackgroundResource(0);
+
+					if(isPointInsideView(event.getRawX(), event.getRawY(), purplelayout))
+					{
+						Toast.makeText(getActivity(), "purple", Toast.LENGTH_SHORT).show();
+						Projection projection = map.getProjection();
+
+						//Convert Points to on screen location
+						LatLng point = projection.fromScreenLocation(new Point((int)event.getX(), (int)event.getY()));
+						comm.returnRoutes(point);
+					}
+					if(isPointInsideView(event.getRawX(), event.getRawY(), bluelayout))
+					{
+						Toast.makeText(getActivity(), "blue", Toast.LENGTH_SHORT).show();
+						int[] location = new int[2];
+						v.getLocationOnScreen(location);
+						Point p = new Point();
+						p.x = location[0];
+						p.y = location[1];
+						
+						nearbyPopUp(getActivity(), p);
+					}
+					if(isPointInsideView(event.getRawX(), event.getRawY(), greylayout))
+					{
+						Toast.makeText(getActivity(), "grey", Toast.LENGTH_SHORT).show();						
+					}
+					
+					
+					((RelativeLayout)v).removeAllViews();
+					
+			    	map.getUiSettings().setScrollGesturesEnabled(true);
+					myHandler.removeCallbacks(r);
+					
+					return true;
+				}
+				
 				return false;
 			}
 			
@@ -220,7 +398,6 @@ public class LocationSearchFragment extends Fragment
 		
 		
         map.setOnInfoWindowClickListener(new InfoWindowClickAdapter(getActivity(), comm));
-        
         
 		//center map on user
 		gps = new GPSTracker(getActivity());
@@ -237,7 +414,7 @@ public class LocationSearchFragment extends Fragment
 		
 		
 		button1 = (ImageButton) getActivity().findViewById(R.id.nearbyMenuButton);
-		button1.setOnClickListener(new OnClickListener() 
+		/*button1.setOnClickListener(new OnClickListener() 
         {
             public void onClick(View v) 
             {
@@ -251,9 +428,10 @@ public class LocationSearchFragment extends Fragment
                  Point point = new Point();
                  point.x = location[0];
                  point.y = location[1];
+                 
                  showStatusPopup(getActivity(), point);
             }
-        });
+        });*/
 
 		
 		
@@ -324,12 +502,13 @@ public class LocationSearchFragment extends Fragment
 	
 	
     // The method that displays the popup.
-	private void showStatusPopup(final Activity context, Point p) 
+	private void nearbyPopUp(final Activity context, Point p) 
 	{
 
 	    // Inflate the popup_layout.xml
 	    LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	    final View layout = layoutInflater.inflate(R.layout.status_popup_layout, null);
+	    
 	    
 	    TextView food = (TextView) layout.findViewById(R.id.foodTextView);
 	    
@@ -447,9 +626,40 @@ public class LocationSearchFragment extends Fragment
 			{			
 				TextView subTextView = (TextView) layout.findViewById(R.id.shoppingTextView);
 				subTextView.setTextColor(context.getResources().getColor(R.color.backgroundblue));				
+				
+				LinearLayout shopSub = (LinearLayout) layout.findViewById(R.id.shoppingSubLayout);
+			    
+			    shopSub.setVisibility(View.VISIBLE);
+			    
+			    if(event.getAction() == MotionEvent.ACTION_MOVE)
+			    {
+			    	TextView shopClothes = (TextView) layout.findViewById(R.id.shoppingTypeClothes);
+					TextView shopShoes = (TextView) layout.findViewById(R.id.shoppingTypeShoes);
+
+					if(isPointInsideView(event.getRawX(), event.getRawY(), layout.findViewById(R.id.shoppingTypeClothes)))
+					{
+						shopClothes.setTextColor(context.getResources().getColor(R.color.backgroundblue));
+					}
+					else
+					{
+						shopClothes.setTextColor(context.getResources().getColor(R.color.purple));
+					}
+					
+					if(isPointInsideView(event.getRawX(), event.getRawY(), layout.findViewById(R.id.shoppingTypeShoes)))
+					{
+						shopShoes.setTextColor(context.getResources().getColor(R.color.backgroundblue));
+					}
+					else
+					{
+						shopShoes.setTextColor(context.getResources().getColor(R.color.purple));
+					}
+					
+					return false;
+			    }
+				
 				if(event.getAction() == MotionEvent.ACTION_UP)
 				{
-					if(isPointInsideView(event.getRawX(), event.getRawY(), layout.findViewById(R.id.shoppingTextView)))
+					if(isPointInsideView(event.getRawX(), event.getRawY(), layout.findViewById(R.id.shoppingTypeClothes)))
 					{
 						if(geoTask.getStatus() == AsyncTask.Status.FINISHED)
 		                {
@@ -457,12 +667,28 @@ public class LocationSearchFragment extends Fragment
 		                }
 		                if(geoTask.getStatus() == AsyncTask.Status.PENDING)
 		                {
-		                	geoTask.setLocation("shopping nearby");
+		                	geoTask.setLocation("clothes shopping nearby");
 		                	geoTask.execute();
 		                }
 		                changeStatusPopUp.dismiss();
 					}
+					if(isPointInsideView(event.getRawX(), event.getRawY(), layout.findViewById(R.id.shoppingTypeShoes)))
+					{
+						if(geoTask.getStatus() == AsyncTask.Status.FINISHED)
+		                {
+		                	geoTask = new AddressSearchAsyncTask(getActivity(), map, searchProgress, addressSearchButton);
+		                }
+		                if(geoTask.getStatus() == AsyncTask.Status.PENDING)
+		                {
+		                	geoTask.setLocation("shoes shopping nearby");
+		                	geoTask.execute();
+		                }
+		                changeStatusPopUp.dismiss();
+					}
+
 					subTextView.setTextColor(context.getResources().getColor(R.color.purple));				
+					shopSub.setVisibility(View.GONE);
+
 					return true;
 				}
 				return false;
@@ -554,20 +780,18 @@ public class LocationSearchFragment extends Fragment
 	    // Creating the PopupWindow
 	    changeStatusPopUp = new PopupWindow(context);
 	    changeStatusPopUp.setContentView(layout);
-	    changeStatusPopUp.setWidth(LinearLayout.LayoutParams.FILL_PARENT);
-	    changeStatusPopUp.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
+	    changeStatusPopUp.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
+	    changeStatusPopUp.setHeight(LinearLayout.LayoutParams.MATCH_PARENT);
 	    changeStatusPopUp.setFocusable(true);
-	
-	    // Some offset to align the popup a bit to the left, and a bit down, relative to button's position.
-	    int OFFSET_X = -20;
-	    int OFFSET_Y = button1.getHeight();
 	
 	    //Clear the default translucent background
 	    changeStatusPopUp.setBackgroundDrawable(new BitmapDrawable());
 	
 	    // Displaying the popup at the specified location, + offsets.
-	    changeStatusPopUp.showAtLocation(layout, Gravity.NO_GRAVITY, p.x + OFFSET_X, p.y + OFFSET_Y);
+	    changeStatusPopUp.showAtLocation(touch, Gravity.NO_GRAVITY, p.x, p.y);
 	}
+	
+	
 	public static boolean isPointInsideView(float x, float y, View view){
 	    int location[] = new int[2];
 	    view.getLocationOnScreen(location);
